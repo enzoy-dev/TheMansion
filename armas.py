59e4ac0b32c8d4a784f35b9943517678db47d943
Enzo <enzosousasantos08@gmail.com>
2026-09-05 22:50:52 -0300

    corrige identação e erros no sistema de armas

--- armas.py
+++ armas.py
@@ -1,10 +1,7 @@
-
-
 import random
 
 
 class Arma:
-
     def __init__(
         self,
         nome: str,
@@ -13,45 +10,35 @@ class Arma:
         chance_acerto: int,
         usa_municao: bool = False
     ) -> None:
-
         self.nome = nome
         self.dano_min = dano_min
         self.dano_max = dano_max
         self.chance_acerto = chance_acerto
         self.usa_municao = usa_municao
 
-
-    def atacar(self, alvo):
+    def atacar(self, alvo) -> int:
         acerto = random.randint(1, 100)
         if acerto <= self.chance_acerto:
             dano = random.randint(self.dano_min, self.dano_max)
-
             print(f"\nVocê acertou o ataque e causou {dano} de dano!")
-
             alvo.receber_dano(dano)
             return dano
+        print("\nVocê errou o ataque!")
         return 0
-    
-    @staticmethod
-    def mostrar_armas() -> None:
-        print("\nArmas disponíveis:")
 
-        for nome, arma in armas.items():
-            print(f"- {nome}: "f"Dano {arma.dano_min}-{arma.dano_max}, Chance de acerto {arma.chance_acerto}%)")
-    
+
 class Faca(Arma):
     def __init__(self) -> None:
         super().__init__(
-    nome="faca",
-    dano_min=10,
-    dano_max=20,
-    chance_acerto=80,
-    usa_municao=False
-)
+            nome="faca",
+            dano_min=10,
+            dano_max=20,
+            chance_acerto=80,
+            usa_municao=False
+        )
 
 
 class Pistola(Arma):
-
     def __init__(self) -> None:
         super().__init__(
             nome="pistola",
@@ -69,5 +56,3 @@ armas = {
     "faca": faca,
     "pistola": pistola
 }
-
-arma = armas.get("faca")
