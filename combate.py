7127e7128ba66f174abca2e6844a455383ba28d0
Enzo <enzosousasantos08@gmail.com>
2026-09-05 22:53:16 -0300

    refactor: limpa combate e padroniza entrada de dados

--- combate.py
+++ combate.py
@@ -1,25 +1,26 @@
+from utilidades import pedir_escolha
+
+
 def combate(jogador, inimigo):
     while jogador.esta_vivo() and inimigo.esta_vivo():
-
         print("\nO que você deseja fazer agora?")
+
         if jogador.arma_equipada:
             print(f"1 - Atacar com {jogador.arma_equipada.nome}")
         else:
-         print("1 - Atacar com a faca")
-         
+            print("1 - Atacar (sem arma equipada)")
+
         print("2 - Usar bandagem")
 
-        escolhaluta = input("Digite o número da sua escolha: ")
+        escolha = pedir_escolha("Digite o número da sua escolha: ")
 
-        if escolhaluta == "1":
-                jogador.atacar(inimigo)
-            
-        elif escolhaluta == "2":
+        if escolha == 1:
+            jogador.atacar(inimigo)
+        elif escolha == 2:
             if jogador.usar_item("bandagem"):
                 jogador.curar(40)
             else:
                 print("\nVocê não tem bandagens suficientes.")
-
         else:
             print("\nOpção inválida.")
 
